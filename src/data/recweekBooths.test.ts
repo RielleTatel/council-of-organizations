b2dc22c7efc4f517b82c411108398acb9a7733f4
rielletatel <tatelgabrielle19@gmail.com>
2026-07-31 08:26:39 +0800

    fix: correct RecWeek map's NFJPIA booth to just JPIA

--- src/data/recweekBooths.test.ts
+++ src/data/recweekBooths.test.ts
@@ -5,7 +5,7 @@ import { organizations } from './organizations'
 const allBooths: BoothShape[] = venues.flatMap((v) => v.booths)
 
 const UNMATCHED = new Set([
-  'ALMS', 'ICPEP', 'NFJPIA', 'FAST', 'ISSOA',
+  'ALMS', 'ICPEP', 'FAST', 'ISSOA',
 ])
 
 describe('recweek venues', () => {
